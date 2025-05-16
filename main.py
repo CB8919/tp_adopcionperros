6d6b7688af19bab90022204aa210f46903fa0e68
Christian <chris.89.19@hotmail.com>
2025-05-16 00:13:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from claseSistAdopcion import Sistem_Adopcion
+
+
+
 print("     ---------- Bienvenidos a ADOPDOG ---------- ")
-print("\n * Donde una nueva historia comienza con un ladrido *\n\n\nEn este sitio vas a encontrar perros que buscan un hogar.\nSon cariñosos, únicos y están esperando una familia como la tuya.\nAdoptar es fácil y cambia vidas, la de ellos... y la tuya también.")
+print("\n * Donde una nueva historia comienza con un ladrido *\n\n\nEn este sitio vas a encontrar perros que buscan un hogar.\nSon cariñosos,  únicos y están esperando una familia como la tuya.\nAdoptar es fácil y cambia vidas, la de ellos... y la tuya también.")
+    
+print("\n\nSeleccione la opcion desea:\n\n.1 Cargar perro\n.2 Registro de usuarios\n.3 Postular un perro\n.4 Confirmar una adopción\n.5 Preferencia de perros\n.6 Listado de perros\n.7 Salir")
+
+       
 
 
-print("\n\nSeleccione la opcion desea:\n\n.1 Cargar perro\n.2 Registro de usuarios\n.3 Postular un perro\n.4 Confirmar una adopción\n.5 Preferencia de perros\n.6 Listado de perros")
 
