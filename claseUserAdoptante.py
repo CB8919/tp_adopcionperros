9a8cc4881dad335d4984fdeccd4584da2f9c9365
Christian <chris.89.19@hotmail.com>
2025-05-18 01:21:05 -0300

    Update claseUserAdoptante.py

--- claseUserAdoptante.py
+++ claseUserAdoptante.py
@@ -1,82 +1,49 @@
+
+
 class User_Adoptante(object):
-    def __init__(self, nombre, dni, email, preferencias, histo_adop):
+    def __init__(self, nombre, dni, email, preferencias):
         self.nombre = nombre
         self.dni = dni
         self.email = email
-        self.preferencias = preferencias # Raza, edad, tamaño
-        self.histo_adop = histo_adop
+        self.preferencias = preferencias # {Raza, edad(cachorro/joven/adulto), tamaño}
+        self.histo_adop = []
         
-
-    def mostrar_user(self):
-        print(f"Nombre: {self.nombre}\nDNI: {self.dni}\nEmail: {self.email}\nPreferncias: {self.preferencias}\nHistorial de adopción: {self.historial()}")
-
         # Método para registrar a un usuario
-    def registro_user(self):
-        print("---- REGISTRO DE USUARIO ----")
-        
-        print("Ingrese nombre, dni, email y preferncias.\nFavor de respetar el orden al ingresar los datos solicitados)")
-        self.nombre = input("Nombre: ")
-        self.dni = input("DNI: ")
-        self.email = input("Email: ")
-
-        print("Ingresar preferencias:")
-        raza = input("Raza preferida: ")
-        tamano = input("Tamaño preferido (grande, mediano, pequeño): ")
-        edad = input("Edad preferida (cachorro, joven, adulto): ")
-
-        self.preferencias = {
-            "raza": raza,
-            "tamaño": tamano,
-            "edad": edad
-        }
-
-        print("\n----- Datos del usuario -----\n\n")
-        print(f"Nombre: {self.nombre}\nDNI: {self.dni}\nEmail: {self.email}\nPreferncias: {self.preferencias}\nHistorial de adopción: {self.historial()}")
-
-        # Método para modificar, actualizar o borrar los datos del usuario
-    def modificar_datos(self):
-        respuesta = "SI"
-        while respuesta == "SI":
-            print("Que dato desea modificar?")
-            print("\nSeleccione:")  
-            print("Nombre: 1\nDNI: 2\nEmail: 3\nPreferencias: 4")
-            try:
-
-                opcion = int(input("Opción:"))
-        
-        
-                if opcion == 1:
-                    self.nombre = input("Nuevo Nombre: ")
-                elif opcion == 2:
-                    self.dni = input("Nuevo DNI: ")
-                elif opcion == 3:
-                    self.email = input("Nuevo email: ")
-                elif opcion == 4:
-                    raza = input("Nueva raza: ")
-                    tamano = input("Nuevo tamaño: ")
-                    edad = input("Nueva edad:")
-
-                    self.preferencias = {
-                        "raza": raza,
-                        "tamaño": tamano,
-                        "edad": edad
-                    }
-                else:
-                    print("Porfavor ingrese 1, 2, 3 o 4 de lo contrario no podra modicar ningún dato")
-                    continue # Para que vuelva al inicio del bucle
-            except ValueError:
-                print("Ingrese un número 1 2 3 o 4")
-                continue # Para que vuelva al inicio del bucle
+    def registrarse(self):
+        return f"Usuario registrado: {self.nombre}\nDNI: {self.dni}\nEmail: {self.email}\nPreferencias: {self.preferencias}\nHistorial de adopción: {self.historial()}"
     
-            print("Quiere modificar otro dato?")
-            respuesta = input("SI/NO").upper()
+        # Método para modificar datos
+    def modificar_datos(self, nuevo_nombre = None, nuevo_email = None, nueva_preferencia = None): #None para omitir si algún dato no se quiere modificar
+        if nuevo_nombre:
+            self.nombre = nuevo_nombre
+        if nuevo_email:
+            self.email = nuevo_email
+        if nueva_preferencia:    
+            self.preferencias = nueva_preferencia
+
+
+        return f"Datos modificados:\nNombre: {nuevo_nombre}\nDNI: {self.dni}\nEmail: {nuevo_email}\nPreferencias: {nueva_preferencia}"
+        # Método para borrar los datos
+    def borrar_datos(self):
+        self.nombre = ""
+        self.dni = ""
+        self.email = ""
+        self.preferencias = {}
+        self.histo_adop = []
+
+        
+        return f"Los datos fueron borrados correctamente"
+        
 
         # Método para mostrar su historial de adopción si lo tiene, o simplemente mostrar todos los datos del usuario
     def historial(self):
-        if self.histo_adop != []:
+        if self.histo_adop == []:
             return "No hay adopciones realizadas"
         else:
-            return "Historial de adopciones: "
+            return f"Historial de adopciones: {self.histo_adop} "
+        
+    def mostrar_user(self):
+        print(f"Nombre: {self.nombre}\nDNI: {self.dni}\nEmail: {self.email}\nPreferencias: {self.preferencias}\nHistorial de adopción: {self.historial()}")
 
 
 
@@ -85,3 +52,5 @@ class User_Adoptante(object):
 
 #nuevo_usuario = User_Adoptante("Carlos", "32564789", "c@gmail.com", {"Labrador", "grande", "cachorro"}, ["Plaga"])
 #nuevo_usuario.modificar_datos()
+
+
