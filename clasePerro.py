82cd93c569cb0b6c42ac6d35b3a541be6caec591
Christian <chris.89.19@hotmail.com>
2025-05-18 01:21:13 -0300

    Update clasePerro.py

--- clasePerro.py
+++ clasePerro.py
@@ -1,14 +1,17 @@
 class Perro(object):
-    def __init__(self, id, nombre, raza, edad, tamano, peso, estado_salud, vacunado, estado, temperamento):
-        self.id = int(id)
+    id_contador = 1 # Para aumentar el id del perro con cada perro nuevo que ingresa
+
+    def __init__(self, id_perro, nombre, raza, edad, tamano, peso, salud, vacunado, temperamento):
+        self.id = Perro.id_contador
+        Perro.id_contador = Perro.id_contador + 1
         self.nombre = nombre
         self.raza = raza
         self.edad = edad # cachorro, joven, adulto
         self.tamano = tamano  # Grande, mediano,pequeño
         self.peso = float(peso)
-        self.estado_salud = estado_salud
+        self.salud = salud
         self.vacunado = vacunado # True o False
-        self.estado = estado # Disponible, reservado, adoptado
+        self.estado_de_adopcion = "Disponible" # Disponible, reservado, adoptado
         self.temperamento = temperamento
         
 
@@ -16,21 +19,16 @@ class Perro(object):
     def cambiar_estado(self, nuevo_estado):
         #if nuevo_estado == "disponible" or nuevo_estado == "reservado" or nuevo_estado == "adoptado":
         if nuevo_estado in ["disponible", "reservado", "adoptado"]:
-            self.estado = nuevo_estado
-            return f"Nuevo estado: {self.estado}"
+            self.estado_de_adopcion = nuevo_estado
+            return f"Nuevo estado: {self.estado_de_adopcion}"
         else:
             return("Estado no valido")
 
         # Métedo para mostrar la info del perro
     def mostrar_info(self):
-        return f"ID: {self.id}\nNombre: {self.nombre}\nRaza: {self.raza}\nEdad: {self.edad}\nTamaño: {self.tamano}\nPeso: {self.peso}kg\nEstado de saludo: {self.estado_salud}\nVacunado: {self.vacunado}\nEstado: {self.estado}\nTemperamento: {self.temperamento}"
+        return (f"ID: {self.id}\nNombre: {self.nombre}\nRaza: {self.raza}\nEdad: {self.edad}\nTamaño: {self.tamano}\nPeso: {self.peso}kg\nSalud: {self.salud}\nVacunado: {self.vacunado}\nEstado: {self.estado_de_adopcion}\nTemperamento: {self.temperamento}")
 
 
-perro = Perro(1, "Rex", "Pastor Alemán", "cachorro", "Grande", 30.5, "Bueno", True, "Disponible", "Amigable")
 
 
-#print(perro.mostrar_info())
-#perro.cambiar_estado("adoptado")
-#print("\nInformación actualizada:")
-#print(perro.mostrar_info())
 
