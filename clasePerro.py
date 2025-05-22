0d285afe0306ed610610df87c880ef7c137846b7
Christian <chris.89.19@hotmail.com>
2025-05-21 21:42:26 -0300

    Update clasePerro.py

--- clasePerro.py
+++ clasePerro.py
@@ -1,9 +1,9 @@
 class Perro():
-    id_contador = 1 # Para aumentar el id del perro con cada perro nuevo que ingresa
+    contador = 1 # Para aumentar el id del perro con cada perro nuevo que ingresa
 
     def __init__(self, nombre, raza, edad, tamano, peso, salud, vacunado, temperamento): #id_perro
-        self.id = Perro.id_contador
-        Perro.id_contador = Perro.id_contador + 1
+        self.id = Perro.contador
+        Perro.contador = Perro.contador + 1
         self.nombre = nombre
         self.raza = raza
         self.edad = edad # cachorro, joven, adulto
@@ -29,12 +29,8 @@ class Perro():
         return (f"ID: {self.id}\nNombre: {self.nombre}\nRaza: {self.raza}\nEdad: {self.edad}\nTamaño: {self.tamano}\nPeso: {self.peso}kg\nSalud: {self.salud}\nVacunado: {self.vacunado}\nEstado: {self.estado_de_adopcion}\nTemperamento: {self.temperamento}")
 
 
-perro1 = Perro(nombre="Max", raza="Labrador", edad="adulto", tamano="grande", peso=30, salud="buena", vacunado=True, temperamento="calmado")
-perro2 = Perro(nombre="Luna", raza="Beagle", edad="joven", tamano="mediano", peso=15, salud="excelente", vacunado=False, temperamento="activo")
-perro3 = Perro(nombre="Coco", raza="Bulldog", edad="cachorro", tamano="pequeño", peso=8, salud="regular", vacunado=True, temperamento="tranquilo")
-print(perro1.mostrar_info())
-print(perro2.mostrar_info())
-print(perro3.mostrar_info())
+
+
 
 
 
